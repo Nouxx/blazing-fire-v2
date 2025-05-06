16c938f520e1b8eba013a451c45b64235cf3b69a
Nouxx <clement.vannouque@gmail.com>
2025-05-06 23:11:57 +0200

    add Flex component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { AppLogo } from "../AppLogo/AppLogo"; // todo: use alias import
 import { HeaderAuth } from "../HeaderAuth/HeaderAuth";
 import { ThemeSwitcher } from "../ThemeSwitcher/ThemeSwitcher";
 import type { User } from "@supabase/supabase-js";
+import { Flex } from "@/components/ui/Flex/Flex";
 
 type HeaderProps = {
   user: User | null;
@@ -13,15 +14,15 @@ type HeaderProps = {
 export const Header = ({ user }: HeaderProps) => {
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-      <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+      <Flex fullWidth className="max-w-5xl p-3 px-5 text-sm">
         <Link href={"/"}>
           <AppLogo />
         </Link>
-        <div className="flex flex-row gap-3">
+        <Flex className="gap-3">
           <HeaderAuth user={user} />
           <ThemeSwitcher />
-        </div>
-      </div>
+        </Flex>
+      </Flex>
     </nav>
   );
 };
---
+++ components/ui/Flex/Flex.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
+
+const flexVariants = cva("flex", {
+  variants: {
+    direction: {
+      column: "flex-col",
+      row: "flex-row",
+    },
+    justify: {
+      between: "justify-between",
+    },
+    align: {
+      center: "items-center",
+    },
+    fullWidth: {
+      false: null,
+      true: "w-full",
+    },
+  },
+  defaultVariants: {
+    direction: "row",
+    justify: "between",
+    align: "center",
+    fullWidth: false,
+  },
+});
+
+interface FlexProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof flexVariants> {}
+
+export const Flex = ({
+  className,
+  direction,
+  justify,
+  align,
+  fullWidth,
+  children,
+  ...props
+}: FlexProps) => {
+  return (
+    <div
+      className={cn(
+        flexVariants({ direction, justify, align, fullWidth, className }),
+      )}
+    >
+      {children}
+    </div>
+  );
+};
