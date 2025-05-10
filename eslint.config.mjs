44cc7aeacf8f2366a9ccb2c3c577199c0775609a
Nouxx <clement.vannouque@gmail.com>
2025-05-11 00:31:49 +0200

    ditch next lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,24 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
+import { includeIgnoreFile } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from "eslint/config";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
+  includeIgnoreFile(gitignorePath),
+  globalIgnores(["postcss.config.js", "tailwind.config.ts"]),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintPluginUnicorn.configs.recommended,
   {
