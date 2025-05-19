2ce9b21a7f1b5a192485e9295397718445806650
Nouxx <clement.vannouque@gmail.com>
2025-05-19 19:49:13 +0200

    improve script logging

--- tools/scripts/setup-env.sh
+++ tools/scripts/setup-env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# ascii art: https://patorjk.com/software/taag font: Big
 
 #    _____ ____  _   _ ______ _____ _____
 #   / ____/ __ \| \ | |  ____|_   _/ ____|
@@ -12,15 +13,35 @@ OUTPUT_FILE_NAME=".env.local"
 OUTPUT_FILE_PATH="$SCRIPT_LOCATION/../../$OUTPUT_FILE_NAME"
 BACKUP_FILE_PATH="$SCRIPT_LOCATION/../../.env.backup"
 
+#    _____ ______ _______ _    _ _____
+#   / ____|  ____|__   __| |  | |  __ \
+#  | (___ | |__     | |  | |  | | |__) |
+#   \___ \|  __|    | |  | |  | |  ___/
+#   ____) | |____   | |  | |__| | |
+#  |_____/|______|  |_|   \____/|_|
+
 echo -n "Checking supabase CLI... "
 if ! supabase --version &> /dev/null; then
   echo "Supabase CLI not found. Please install it from https://supabase.com/docs/guides/cli"
   exit 1
 fi
-echo "ok"
+echo "ok."
 
-# create backup of the existing .env.local file
-cp $OUTPUT_FILE_PATH $BACKUP_FILE_PATH
+echo -n "Creating backup file... "
+if [ -f "$OUTPUT_FILE_PATH" ]; then
+  cp "$OUTPUT_FILE_PATH" "$BACKUP_FILE_PATH"
+  echo "ok."
+else
+  echo "skipped."
+fi
+
+echo -n "Creating output file... "
+if [ ! -f "$OUTPUT_FILE_PATH" ]; then
+  touch $OUTPUT_FILE_PATH
+  echo "ok."
+else
+  echo "skipped."
+fi
 
 #    _____ _    _ _____        ____           _____ ______
 #   / ____| |  | |  __ \ /\   |  _ \   /\    / ____|  ____|
@@ -36,6 +57,6 @@ supabase status -o env \
   --override-name auth.anon_key=NEXT_PUBLIC_SUPABASE_ANON_KEY \
   > $OUTPUT_FILE_PATH
 
-echo "Supabase ok"
+echo "Supabase ok."
 
 echo "Successfully generated $OUTPUT_FILE_NAME!"
